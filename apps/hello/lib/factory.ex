7bfc932ae9b6c14754590091089d126cb13253a9
Samuel Volin <untra.sam@gmail.com>
2017-05-23 14:57:03 -0600

    update newlines

--- apps/hello/lib/factory.ex
+++ apps/hello/lib/factory.ex
@@ -5,12 +5,28 @@ defmodule Hello.Factory do
   alias Faker.Avatar
   alias Faker.Name.En
   alias Hello.User
+  alias Hello.List
+  alias Hello.Item
   use ExMachina.Ecto, repo: Hello.Repo
 
   def user_factory do
-    %Hello.User{
+    %User{
       name: En.name(),
       avatar_url: Enum.random([nil, Avatar.image_url()])
+    }
+  end
+
+  def list_factory do
+    %List{
+      name: Faker.Lorem.word()
+      notes: Faker.Lorem.sentence()
+    }
+  end
+
+  def item_factory do
+    %Item{
+      name: Faker.Lorem.word(),
+      image_url: Enum.random([nil, Avatar.image_url()])
     }
   end
 end
